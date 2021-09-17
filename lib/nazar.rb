7f1e4939b5d32415fd4f8c82e07418a5a8c0396b
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2021-09-17 17:26:19 +0200

    Add Pry support

--- lib/nazar.rb
+++ lib/nazar.rb
@@ -25,17 +25,43 @@ module Nazar
   end
 
   def self.enable!
-    if defined?(IRB)
-      ::IRB::Irb.class_eval do
-        alias_method :__original_output_value__, :output_value
-        def output_value(omit = false)
-          ::Nazar::Renderer.new(@context.last_value).render || __original_output_value__(omit)
-        end
+    return if @enabled
+
+    enable_for_irb! if defined?(IRB)
+    enable_for_pry! if defined?(Pry)
+
+    @enabled = true
+  end
+
+  def self.enable_for_irb!
+    ::IRB::Irb.class_eval do
+      alias_method :__original_output_value__, :output_value
+      def output_value(omit = false) # rubocop:disable Style/OptionalBooleanParameter
+        ::Nazar::Renderer.new(@context.last_value).render || __original_output_value__(omit)
       end
     end
   end
 
+  def self.enable_for_pry!
+    @__original_pry_print = Pry.config.print
+    Pry.config.print = proc do |output, value|
+      output.puts Nazar::Renderer.new(value).render!
+    end
+  end
+
   def self.disable!
+    return unless @enabled
+
+    disable_for_irb! if defined?(IRB)
+
+    @enabled = false
+  end
+
+  def self.disable_for_irb!
     ::IRB::Irb.send(:alias_method, :output_value, :__original_output_value__)
+  end
+
+  def self.disable_for_pry!
+    nil
   end
 end
