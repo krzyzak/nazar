2bf661e8f0556301d05f54ada9bd3f17d5874f88
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2021-09-21 10:31:23 +0200

    Refactor Nazar class methods a bit

--- lib/nazar.rb
+++ lib/nazar.rb
@@ -24,65 +24,67 @@ module Nazar
     setting :enabled, default: ENV.fetch('ENABLE_TTY_COLORS') { TTY::Color.color? ? 'true' : 'false' } == 'true'
   end
 
-  def self.extensions
-    @extensions ||= Set.new
-  end
+  class << self
+    def extensions
+      @extensions ||= Set.new
+    end
 
-  def self.enable!(extensions: [:active_record, :csv])
-    return if @enabled
+    def enable!(extensions: [:active_record, :csv])
+      return if @enabled
 
-    load_active_record! if extensions.include?(:active_record) || defined?(ActiveRecord)
+      load_active_record! if extensions.include?(:active_record) || defined?(ActiveRecord)
 
-    enable_for_irb! if defined?(IRB)
-    enable_for_pry! if defined?(Pry)
+      enable_for_irb! if defined?(IRB)
+      enable_for_pry! if defined?(Pry)
 
-    @enabled = true
-  end
+      @enabled = true
+    end
 
-  def self.load_csv!
-    extensions << :csv
+    def load_csv!
+      extensions << :csv
 
-    require 'csv'
-    require 'nazar/formatter/csv_table'
-  end
+      require 'csv'
+      require 'nazar/formatter/csv_table'
+    end
 
-  def self.load_active_record!
-    extensions << :active_record
+    def load_active_record!
+      extensions << :active_record
 
-    require 'active_record'
-    require 'nazar/formatter/active_record_collection'
-    require 'nazar/formatter/active_record_item'
-  end
+      require 'active_record'
+      require 'nazar/formatter/active_record_collection'
+      require 'nazar/formatter/active_record_item'
+    end
 
-  def self.enable_for_irb!
-    ::IRB::Irb.class_eval do
-      alias_method :__original_output_value__, :output_value
-      def output_value(omit = false) # rubocop:disable Style/OptionalBooleanParameter
-        ::Nazar::Renderer.new(@context.last_value).render || __original_output_value__(omit)
+    def enable_for_irb!
+      ::IRB::Irb.class_eval do
+        alias_method :__original_output_value__, :output_value
+        def output_value(omit = false) # rubocop:disable Style/OptionalBooleanParameter
+          ::Nazar::Renderer.new(@context.last_value).render || __original_output_value__(omit)
+        end
       end
     end
-  end
 
-  def self.enable_for_pry!
-    @__original_pry_print = Pry.config.print
-    Pry.config.print = proc do |output, value, instance|
-      output.puts Nazar::Renderer.new(value).render || @__original_pry_print.call(output, value, instance)
+    def enable_for_pry!
+      @__original_pry_print = Pry.config.print
+      Pry.config.print = proc do |output, value, instance|
+        output.puts Nazar::Renderer.new(value).render || @__original_pry_print.call(output, value, instance)
+      end
     end
-  end
 
-  def self.disable!
-    return unless @enabled
+    def disable!
+      return unless @enabled
 
-    disable_for_irb! if defined?(IRB)
+      disable_for_irb! if defined?(IRB)
 
-    @enabled = false
-  end
+      @enabled = false
+    end
 
-  def self.disable_for_irb!
-    ::IRB::Irb.send(:alias_method, :output_value, :__original_output_value__)
-  end
+    def disable_for_irb!
+      ::IRB::Irb.send(:alias_method, :output_value, :__original_output_value__)
+    end
 
-  def self.disable_for_pry!
-    nil
+    def disable_for_pry!
+      nil
+    end
   end
 end
